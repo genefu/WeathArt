df996f79394ef81b9d93d31d475edc36a9bc760f
genefu <genesis_fu@yahoo.com>
2023-10-14 20:43:02 -0400

    Adds the other weathers

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -23,6 +23,7 @@ function setup() {
   // put setup code here
   createCanvas(screen.availWidth, screen.availHeight);
   noFill();
+  frameRate(15);
 
   //initialize spiral colors
   for (let i = 0; i < random(25, 50); i++) {
@@ -47,8 +48,8 @@ function setup() {
 
 function draw() {
   //background color based on the time
-  //background(200 - (12 * time), 180 - (15 * time), 100 + (8 * time), 100);
-  background('rgba(100,150,220,0.05)');
+  background(200 - (12 * time), 180 - (15 * time), 100 + (8 * time));
+  //only if rain background('rgba(100,150,220,0.05)');
   translate(width / 2, height / 2);
   /*for (let z = -screen.width; z < screen.width; z+= 100) {
     for (let y = -height; y < 2000; y += 10) {
@@ -65,6 +66,7 @@ function draw() {
     }
   }*/
   //switch case for diff weathers
+  //sunny
   /*for (let spiral of spirals) {
     push();
     translate(spiral.x, spiral.y);
@@ -82,46 +84,60 @@ function draw() {
   }*/
 
   //snow
-  /*let angle = 0.0;
-  let offset = Math.round(Math.random() * 100);
-  let scalar = 2;
-  let speed = 0.05;
+  /*var angle = 0.0;
+  var offset = -width;
+  var scalar = 2;
+  var speed = 0.5;
 
-  for (let i = offset; i < screen.width / 2; i += 1000) {
-    let x = i + cos(angle) * scalar;
-    let y = i + sin(angle) * scalar;
+  for (let i = offset; i < screen.width / 2; i += 1) {
+    var x = i + cos(angle) * scalar;
+    var y = i + sin(angle) * scalar;
+    ellipse(x, y, 2, 2);
+    ellipseMode(CENTER);
+    angle += speed;
+    scalar += speed;
+    stroke('white');
+    strokeWeight(Math.random() * 5);
+    speed = Math.random() + 0.01;
+  }*/
+
+  //stars
+  /*for (let i = offset; i < screen.width/2; i += 1) {
+    var x = i + cos(angle) * scalar;
+    var y = i + sin(angle) * scalar;
     ellipse(x, y, 2, 2);
     ellipseMode(CENTER);
     angle += speed;
     scalar += speed;
     stroke('white');
     strokeWeight(Math.random() * 5)
-    speed = Math.random() + 0.01;
+    //speed = Math.random() + 0.01;
   }*/
 
   //rain
+  /*
   let lines = [];
   let reflections = [];
 
-  if (random(1) > 0.75) {
-    let x = random(screen.availWidth);
-    let y = 0;
-    let len = random(5,60);
-    let opacity = random(160,200);
+  if (random(1) > 0.2) {
+    let x = random(-screen.availWidth, screen.availWidth);
+    let y = random(-screen.availHeight, screen.availHeight);
+    let len = random(50, 100);
+    let opacity = random(160, 200);
     let weight = map(len, 5, 10, 0.5, 2);
-    let blueShade = random(180,220);
-    lines.push({x,y,len,blueShade,opacity,weight});
+    let blueShade = random(180, 220);
+    lines.push({ x, y, len, blueShade, opacity, weight });
   }
 
   if (random(1) < 0.03) {
-    let x = random(screen.availWidth);
-    let y = 0;
+    let x = random(-screen.availWidth, screen.availWidth);
+    let y = random(-screen.availHeight, screen.availHeight);
     let len = random(1, 50);
-    let opacity = random(200,250);
-    let weight = map(len,5,10,0.5,2);
-    let reflectColor = color(random(200,250),random(200,255),random(200,250));
-    reflections.push({x,y,len,reflectColor,opacity,weight});
-  }
+    let opacity = random(200, 250);
+    let weight = map(len, 5, 10, 0.5, 2);
+    let reflectColor = color(random(200, 250), random(200, 255), random(200, 250));
+    reflections.push({ x, y, len, reflectColor, opacity, weight });
+  } 
 
   for (let i = lines.length - 1; i >= 0; i--) {
     let linee = lines[i];
@@ -143,10 +159,10 @@ function draw() {
     line(reflection.x, reflection.y, reflection.x, reflection.y + reflection.len);
     strokeWeight(reflection.weight);
     reflection.y +=1;
-    if (reflection.y > height) {
+    if (reflection.y > screen.availHeight) {
       reflections.splice(i,1);
     }
-  }
+  } */
 
   //lightning
   /*var x1 = Math.round(Math.random() * width);
@@ -178,4 +194,94 @@ function draw() {
   line(x6, y6, x6 - random(1, 300), y6 - random(1, 300))
   line(x6, y6, x6 + random(1, 300), y6 - random(1, 300))*/
 
-}
+  //partly cloudy
+  /*var z = 0
+  let x = 0;
+  let y = 0;
+  let circles = [];
+  num_circles = screen.availWidth * 250 / 400;
+
+  var x_pos_center = 0;
+  var cloud_length = Math.floor(Math.random() * ((screen.availWidth * 200 / 400) - 0 + (screen.availWidth * 50 / 400))) + (screen.availWidth * 50 / 400);
+  var cloud_height = Math.floor(Math.random() * ((screen.availWidth * 60 / 300) - 0 + (screen.availWidth * 20 / 300))) + (screen.availWidth * 20 / 300);
+  if (circles.length < num_circles){
+  for (let j = cloud_length / 6; j > 0; --j){
+    var rel_pos = (Math.random() - 0.5) * 2;
+    var piece_pos = x_pos_center + (rel_pos * Math.floor(Math.random() * ((cloud_length / 2))));
+    var cloud_size = pow(exp(- (pow(rel_pos, 2))), 2) * 30;
+    cloud_height = cloud_height + (Math.random() - 0.5) * 5;
+    circles.push({piece_pos, cloud_height, cloud_size});
+    }
+  }
+
+  for(let p = circles.length - 1; p >= 0; p--){
+    circles[p].piece_pos = circles[p].piece_pos + 0.5;
+    fill(255, 255, 255, 100);
+    circle(circles[p].piece_pos, circles[p].cloud_height, circles[p].cloud_size);
+
+    if (circles[p].piece_pos > width) {
+      // Remove the circle from the array
+      circles.splice(p, 1);
+    }
+  }*/
+
+  //cloudy
+  /*var z = 0
+  let x = 0;
+  let y = 0;
+  let circles = [];
+  num_circles = 500;
+
+  /*var x_pos_center = 0;
+  var cloud_length = Math.floor(Math.random() * (200 - 0 + 50)) + 50;
+  var cloud_height = Math.floor(Math.random() * (60 - 0 + 20)) + 20;
+  for (let j = cloud_length / 6; j > 0; j--) {
+    var rel_pos = (Math. random() - 0.5) * 2;
+    var piece_pos = x_pos_center + (rel_pos * Math. floor (Math. random() * ((cloud_length / 2))));
+    var cloud_size = pow(exp(- (pow(rel_pos, 2))), 2) * 30;
+    cloud_height = cloud_height + (Math. random() - 0.5) * 5;
+    circles.push({piece_pos, cloud_height, cloud_size});
+  }
+
+  for(let p = circles.length - 1; p >= 0; p--){
+    circles[p].piece_pos = circles[p].piece_pos + 1;
+    fill(255, 255, 255, 100);
+    circle(circles[p].piece_pos, circles[p].cloud_height, circles[p].cloud_size);
+
+    if (circles[p].piece_pos > width) {
+      // Remove the circle from the array
+      circles.splice(p, 1);
+    }
+  }*/
+  
+  //foggy
+  var z = 0;
+  var x = 0;
+  var y = 0;
+  var circles = [];
+  num_circles = screen.availWidth * 6000 / 400;
+
+  var x_pos_center = 100;
+  var cloud_length = screen.availWidth * 2;
+  var cloud_height = screen.availHeight - (screen.availHeight * 1/10);
+  if (circles.length < num_circles){
+  for (let j = cloud_length / 6; j > 0; --j){
+    var rel_pos = (Math. random() - 0.5) * 2;
+    var piece_pos = x_pos_center + (rel_pos * Math. floor (Math.random() * ((cloud_length / 2) - 0 - 0)) - 0);
+    var cloud_size = pow(exp(- (pow(rel_pos, 2))), 2) * 30;
+    cloud_height = cloud_height + (Math.random() - 0.5) * 5;
+    circles.push({piece_pos, cloud_height, cloud_size});
+  }
+  }
+
+  for(let p = circles.length - 1; p >= 0; p--){
+    circles[p].piece_pos = circles[p].piece_pos + 0.2;
+    fill(190, 190, 190, 40);
+    circle(circles[p].piece_pos, circles[p].cloud_height, circles[p].cloud_size);
+
+    if (circles[p].piece_pos > width) {
+      // Remove the circle from the array
+      circles.splice(p, 1);
+    }
+  }
+} 
